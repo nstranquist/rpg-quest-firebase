972c0b769cfee54d52c2c1ef1c5803c45b4a26c5
nicostran <nstranquist@gmail.com>
2019-10-11 18:33:46 -0500

    add updatePlayer() cloud function, no security right now

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -16,6 +16,7 @@ interface Player {
   name: string
   lvl: number
   xp: number
+  gold: number
   isDead: boolean
 }
 //interface Monster {}
@@ -23,7 +24,7 @@ interface Player {
 //interface Formulas {}
 
 const getPlayer = (req: express.Request, res: express.Response) => {
-  // check auth (req.params.auth?)
+  // check auth (req.params.auth?), or do it below after getting collection
 
   // get player data from document in /profiles collection
   db.collection('profiles').doc(`${req.params.id}`)
@@ -37,11 +38,34 @@ const getPlayer = (req: express.Request, res: express.Response) => {
         name: data.name,
         lvl: data.lvl,
         xp: data.xp,
+        gold: data.gold,
         isDead: data.isDead
       }
       res.json(playerData)
     })
     .catch(err => res.status(500).json({ error: err.code }))
 }
+// can accept lvl, xp, gold, isDead
+const updatePlayer = (req: express.Request, res: express.Response) => {
+  const playerData = {  // can add error checking here
+    lvl: req.body.lvl,
+    xp: req.body.xp,
+    gold: req.body.gold
+  }
+  //if (playerData && req.body.id === req.params.id) {  // confirm user
+    db.collection('profiles').doc(`${req.params.id}`)
+      .update({
+        lvl: req.body.lvl,
+        xp: req.body.xp,
+        gold: req.body.gold
+      })
+      .then(() => {
+        res.json(playerData)
+      })
+      .catch((err) => res.status(500).json({ error: err.code }))
+  //}
+  //res.status(400).json({ message: 'Inadequate data submitted' })
+}
 
 app.get('/profile/:id', getPlayer)
+app.post('/profile/:id', updatePlayer)
