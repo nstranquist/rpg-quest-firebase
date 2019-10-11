61b5230c5f7ca13fdfd8fdc2ad3be3244ad14670
nicostran <nstranquist@gmail.com>
2019-10-11 18:10:15 -0500

    add getPlayer() function for getting player data given the player's id

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -11,7 +11,37 @@ app.use(cors())
 
 exports.api = functions.https.onRequest(app)
 
-//interface Player {}
+interface Player {
+  id: string
+  name: string
+  lvl: number
+  xp: number
+  isDead: boolean
+}
 //interface Monster {}
 //interface RPG {}
+//interface Formulas {}
 
+const getPlayer = (req: express.Request, res: express.Response) => {
+  // check auth (req.params.auth?)
+
+  // get player data from document in /profiles collection
+  db.collection('profiles').doc(`${req.params.id}`)
+    .get()
+    .then((doc) => {
+      if (!doc.exists)
+        res.status(404).json({ profile: 'Not found' })
+      const data = doc.data()!
+      const playerData: Player = {
+        id: doc.id,
+        name: data.name,
+        lvl: data.lvl,
+        xp: data.xp,
+        isDead: data.isDead
+      }
+      res.json(playerData)
+    })
+    .catch(err => res.status(500).json({ error: err.code }))
+}
+
+app.get('/profile/:id', getPlayer)
